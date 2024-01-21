660de0a0996488eb2fa5fdfbe34bf4ffa2463f8a
= <darts100@gmail.com>
2024-01-22 10:08:22 +1300

    Added headers for server and client with function defs

---
+++ file_client.h
@@ -0,0 +1,3 @@
+int setup_socket(char *ip, int port);
+void sync_file(char *ip, char *port_str, char *filename);
+void transfer_file(char *ip, char *port_str, char *filename);
---
+++ file_server.h
@@ -0,0 +1,4 @@
+void sync_file(int client_socket);
+void transfer_file(int client_socket);
+void *handle_connection(void *p_client);
+void init_server(int port);
